a0288d78f6aad860bacf857f88e613ad972f2e95
rikimaru0345 <dr.pi.chu3@gmail.com>
2019-05-22 09:11:12 +0200

    include all primitives in .FriendlyName()

--- src/Ceras/Helpers/ReflectionHelper.cs
+++ src/Ceras/Helpers/ReflectionHelper.cs
@@ -569,25 +569,29 @@
 
 		public static string FriendlyName(this Type type, bool fullName = false)
 		{
-			if (type == typeof(int))
-				return "int";
-			else if (type == typeof(short))
-				return "short";
-			else if (type == typeof(byte))
-				return "byte";
-			else if (type == typeof(bool))
-				return "bool";
-			else if (type == typeof(long))
-				return "long";
-			else if (type == typeof(float))
-				return "float";
-			else if (type == typeof(double))
-				return "double";
-			else if (type == typeof(decimal))
-				return "decimal";
-			else if (type == typeof(string))
-				return "string";
-			else if (type.IsGenericType)
+			if (type == typeof(bool))  return "bool";
+
+			if (type == typeof(byte))  return "byte";
+			if (type == typeof(sbyte))  return "sbyte";			
+			
+			if (type == typeof(short)) return "short";
+			if (type == typeof(ushort)) return "ushort";
+
+			if (type == typeof(int))	return "int";
+			if (type == typeof(uint))	return "uint";
+			
+			if (type == typeof(long))  return "long";
+			if (type == typeof(ulong))  return "ulong";
+
+			if (type == typeof(float)) return "float";
+			if (type == typeof(double)) return "double";
+
+			if (type == typeof(decimal)) return "decimal";
+			
+			if (type == typeof(string)) return "string";
+			if (type == typeof(char)) return "char";
+			
+			if (type.IsGenericType)
 			{
 				var n = fullName ? type.FullName : type.Name;
 				return n.Split('`')[0] + "<" + string.Join(", ", type.GetGenericArguments().Select(t => t.FriendlyName(fullName)).ToArray()) + ">";
