fb79f24b0e56facb5522e306da1944f6e707b21c
Zach Vinless <zvinless@users.noreply.github.com>
2019-05-23 09:48:09 -0700

    remove unused code from main program

--- src/Ceras.AotGenerator/Program.cs
+++ src/Ceras.AotGenerator/Program.cs
@@ -2,15 +2,11 @@
 
 namespace CerasAotFormatterGenerator
 {
-	using System.Collections.Generic;
-	using Ceras;
 	using System.IO;
 	using System.Linq;
 	using System.Reflection;
 	using System.Text;
 	using System.Threading;
-	using Ceras.Formatters;
-	using Ceras.Formatters.AotGenerator;
 
 	class Program
 	{
@@ -51,84 +47,6 @@ namespace CerasAotFormatterGenerator
 
 			Thread.Sleep(300);
 			Console.WriteLine($"> Done!");
-		}
-
-		static (CerasSerializer, List<Type>) CreateSerializerAndTargets(IEnumerable<Assembly> asms)
-		{
-			// Find config method and create a SerializerConfig
-			SerializerConfig config = new SerializerConfig();
-			var configMethods = asms.SelectMany(a => a.GetTypes())
-									.SelectMany(t => t.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
-									.Where(m => m.GetCustomAttribute<CerasAutoGenConfigAttribute>() != null)
-									.ToArray();
-			if (configMethods.Length > 1)
-				throw new Exception("Found more than one method with the CerasAutoGenConfig attribute!");
-			if (configMethods.Length == 1)
-				config = (SerializerConfig)configMethods[0].Invoke(null, null);
-
-			config.VersionTolerance.Mode = VersionToleranceMode.Disabled; // ensure VersionTolerance is off so we don't accidentally get 'SchemaDynamicFormatter'
-			var ceras = new CerasSerializer(config);
-
-
-			// Start with KnownTypes...
-			HashSet<Type> newTypes = new HashSet<Type>();
-			newTypes.AddRange(config.KnownTypes);
-
-			// And also include all marked types
-			var marker = typeof(CerasAutoGenFormatterAttribute);
-
-			bool HasMarker(Type t) => t.GetCustomAttributes(true)
-									   .Any(a => a.GetType().FullName == marker.FullName);
-
-			var markedTargets = asms.SelectMany(a => a.GetTypes())
-									.Where(t => !t.IsAbstract && HasMarker(t));
-
-			newTypes.AddRange(markedTargets);
-
-
-			// Go through each type, add all the member-types it wants to serialize as well
-			HashSet<Type> allTypes = new HashSet<Type>();
-
-			while (newTypes.Any())
-			{
-				// Get first, remove from "to explore" list, and add it to the "done" list.
-				var t = newTypes.First();
-				newTypes.Remove(t);
-				allTypes.Add(t);
-
-
-				if (CerasSerializer.IsPrimitiveType(t))
-					// Skip int, string, Type, ...
-					continue;
-
-				if (t.IsAbstract || t.ContainsGenericParameters)
-					// Can't explore abstract or open generics
-					continue;
-
-				// Explore the type, add all member types
-				var schema = ceras.GetTypeMetaData(t).PrimarySchema;
-
-				foreach (var member in schema.Members)
-					if (!allTypes.Contains(member.MemberType))
-						newTypes.Add(member.MemberType);
-			}
-
-
-			// Only leave things that use DynamicFormatter, or have the marker attribute
-			List<Type> targets = new List<Type>();
-
-			foreach (var t in allTypes)
-			{
-				var f = ceras.GetSpecificFormatter(t);
-				var fType = f.GetType();
-
-				if (fType.IsGenericType && fType.GetGenericTypeDefinition().Name == typeof(DynamicFormatter<int>).GetGenericTypeDefinition().Name)
-					targets.Add(t);
-				else if (HasMarker(t))
-					targets.Add(t);
-			}
-
-			return (ceras, targets);
 		}
 	}
 }
