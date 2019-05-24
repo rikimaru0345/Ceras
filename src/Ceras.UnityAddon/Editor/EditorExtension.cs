e2331da716af453b08516ed6f258036b35ed8f6e
Zach Vinless <zvinless@users.noreply.github.com>
2019-05-24 10:59:38 -0700

    auto generate on recompile

--- src/Ceras.UnityAddon/Editor/EditorExtension.cs
+++ src/Ceras.UnityAddon/Editor/EditorExtension.cs
@@ -2,23 +2,28 @@ namespace Ceras.Editor
 {
 	using System.IO;
 	using System.Linq;
-	using System.Text;
 	using System.Reflection;
+	using System.Text;
 	using UnityEditor;
+	using UnityEditor.Callbacks;
 	using CerasAotFormatterGenerator;
 
 	public static class CerasUnityEditorTools
 	{
 		const string outputCsFileName = "Assets/Scripts/CerasAotFormattersGenerated.cs";
 
-		[MenuItem("Tools/Ceras/Generate AOT Formatters")]
+		[DidReloadScripts, MenuItem("Tools/Ceras/Generate AOT Formatters")]
 		public static void GenerateAotFormatters()
 		{
 			var executingAssembly = Assembly.GetExecutingAssembly();
 			var asms = executingAssembly.GetReferencedAssemblies().Select(Assembly.Load).Append(executingAssembly);
 			var sb = new StringBuilder(25 * 1000);
 			Generator.Generate(asms, sb);
-			File.WriteAllText(outputCsFileName, sb.ToString());
+			var output = sb.ToString();
+			if (File.Exists(outputCsFileName) && File.ReadAllText(outputCsFileName) == output)
+				return;
+
+			File.WriteAllText(outputCsFileName, output);
 			AssetDatabase.ImportAsset(outputCsFileName);
 		}
 	}
