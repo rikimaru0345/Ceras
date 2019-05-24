2141f6f644517eacd6de37a770236b9a4d81f295
Zach Vinless <zvinless@users.noreply.github.com>
2019-05-24 08:54:48 -0700

    add barebones editor script

---
+++ src/Ceras.UnityAddon/Editor/EditorExtension.cs
@@ -0,0 +1,25 @@
+namespace Ceras.Editor
+{
+	using System.IO;
+	using System.Linq;
+	using System.Text;
+	using System.Reflection;
+	using UnityEditor;
+	using CerasAotFormatterGenerator;
+
+	public static class CerasUnityEditorTools
+	{
+		const string outputCsFileName = "Assets/Scripts/CerasAotFormattersGenerated.cs";
+
+		[MenuItem("Tools/Ceras/Generate AOT Formatters")]
+		public static void GenerateAotFormatters()
+		{
+			var executingAssembly = Assembly.GetExecutingAssembly();
+			var asms = executingAssembly.GetReferencedAssemblies().Select(Assembly.Load).Append(executingAssembly);
+			var sb = new StringBuilder();
+			Generator.Generate(asms, sb);
+			File.WriteAllText(outputCsFileName, sb.ToString());
+			AssetDatabase.ImportAsset(outputCsFileName);
+		}
+	}
+}
