b3083bbc47e5d3b0fa7b1b21b4491d5d305a443f
Rikimaru <rikimaru0345@users.noreply.github.com>
2019-08-09 15:21:27 +0200

    cleanup and update unity EditorExtension

--- src/Ceras.UnityAddon/Editor/EditorExtension.cs
+++ src/Ceras.UnityAddon/Editor/EditorExtension.cs
@@ -15,15 +15,30 @@ namespace Ceras.Editor
 		[DidReloadScripts, MenuItem("Tools/Ceras/Generate AOT Formatters")]
 		public static void GenerateAotFormatters()
 		{
+			// Prepare AotGenerator
+			Generator.RegisterAssemblyResolver();
+
+			// Load all assemblies
 			var executingAssembly = Assembly.GetExecutingAssembly();
 			var asms = executingAssembly.GetReferencedAssemblies().Select(Assembly.Load).Append(executingAssembly);
+
+			// Generate source-code for all formatters
 			var sb = new StringBuilder(25 * 1000);
 			Generator.Generate(asms, sb);
 			var output = sb.ToString();
-			if (File.Exists(outputCsFileName) && File.ReadAllText(outputCsFileName) == output)
-				return;
 
+			// Check if the output is already exactly the same
+			if (File.Exists(outputCsFileName) && File.ReadAllText(outputCsFileName) == output)
+				return; // File didn't change at all, no need to write
+
+			// Ensure target directory exists
+			var dir = Path.GetDirectoryName(outputCsFileName);
+			Directory.CreateDirectory(dir);
+
+			// Write code to .cs file
 			File.WriteAllText(outputCsFileName, output);
+
+			// Tell Unity to reload the file
 			AssetDatabase.ImportAsset(outputCsFileName);
 		}
 	}
